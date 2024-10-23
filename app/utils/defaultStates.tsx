80bec2fb3262fc3a6d74238128394fe77484ec73
Sayan Mondal <101855586+Sayan-sss@users.noreply.github.com>
2024-10-23 12:52:27 +0530

    final

---
+++ app/utils/defaultStates.tsx
@@ -0,0 +1,39 @@
+const defaultStates = [
+  {
+    name: "Kolkata",
+    country: "IN",
+    state: "Kokata",
+    lat: 22.5697,
+    lon: 88.3697,
+  },
+  {
+    name: "Delhi",
+    country: "IN",
+    state: "Delhi",
+    lat: 28.6667,
+    lon: 77.2167,
+  },
+  {
+    name: "Mumbai",
+    country: "IN",
+    state: "Maharashtra",
+    lat: 19.0144,
+    lon: 72.8479,
+  },
+  {
+    name: "Hyderabad",
+    country: "IN",
+    state: "Hyderabad",
+    lat: 17.3753,
+    lon: 78.4744,
+  },
+  {
+    name: "Bangalore",
+    country: "IN",
+    state: "Karnataka",
+    lat: 12.9762,
+    lon: 77.6033,
+  },
+];
+
+export default defaultStates;
